d975c179c416ea9feb362640d47409a965da4c06
Noah Dos Santos <noahpds@proton.me>
2024-08-06 21:14:44 -0700

    Update version string

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     // Release version (e.g. "indev-0.3.2_02")
     // Do NOT increment the major version number until IRC4n is ready for release.
     // Dev stage: 0.major.minor_bugfix
-    public static String version = "indev-0.0.0_00";
+    public static String version = "indev-0.0.1_00";
     public static void main(String[] args) {
         System.out.println("IRC4n version " + version);
         // Ask the interpreter to ask the user to determine if irc4n should be client or server
